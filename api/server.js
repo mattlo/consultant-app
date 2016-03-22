7af628251bef99328c79bc1f39d51ae6b32364e3
Matt Lo <mattlo.developer@gmail.com>
2016-03-22 15:52:30 -0500

    added core dependencies, added server instance, added dev runmode, added babel config

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const Hapi = require('hapi');
+
+// Create a server with a host and port
+const server = new Hapi.Server();
+server.connection({
+  host: 'localhost',
+  port: 8000
+});
+
+// Add the route
+server.route({
+  method: 'GET',
+  path:'/',
+  handler: function (request, reply) {
+    return reply('response test');
+  }
+});
+
+// Start the server
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Server running at:', server.info.uri);
+});
