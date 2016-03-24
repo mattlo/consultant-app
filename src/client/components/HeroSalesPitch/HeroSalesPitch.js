a70112ba6c847cb98e6243e913d91b6c0e0ce335
Matt Lo <mattlo.developer@gmail.com>
2016-03-24 18:16:53 -0500

    added hero background

--- src/client/components/HeroSalesPitch/HeroSalesPitch.js
+++ src/client/components/HeroSalesPitch/HeroSalesPitch.js
@@ -1,10 +1,21 @@
 import './HeroSalesPitch.scss';
 import React from 'react';
+import {isTouchDevice} from '../../util/common';
 
 export default function HeroSalesPitch() {
   return (
     <div className="hero-sales-pitch">
-      Hero Sales Pitch Component
+      <div className="video embed-responsive embed-responsive-16by9">
+        <img src="https://cdn.mattlo.io/pluto.jpg" className="embed-responsive-item" />
+
+        {!isTouchDevice() ? (
+          <video preload autoPlay muted loop className="embed-responsive-item fx">
+            <source src="https://cdn.mattlo.io/pluto.mp4" type="video/mp4" />
+          </video>
+        ) : (
+          <img src="https://cdn.mattlo.io/pluto.gif" className="embed-responsive-item fx" />
+        )}
+      </div>
     </div>
   );
 }
