cde7b88aa8fdf8025ca0ce8b1e99810fae355c28
Matt Lo <mattlo.developer@gmail.com>
2016-03-24 19:41:52 -0500

    fixed linter warnings

--- src/client/components/HeroSalesPitch/HeroSalesPitch.js
+++ src/client/components/HeroSalesPitch/HeroSalesPitch.js
@@ -6,14 +6,14 @@ export default function HeroSalesPitch() {
   return (
     <div className="hero-sales-pitch">
       <div className="video embed-responsive embed-responsive-16by9">
-        <img src="https://cdn.mattlo.io/pluto.jpg" className="embed-responsive-item"/>
+        <img src="https://cdn.mattlo.io/pluto.jpg" className="embed-responsive-item" />
 
         {!isTouchDevice() ? (
           <video preload autoPlay muted loop className="embed-responsive-item fx">
-            <source src="https://cdn.mattlo.io/pluto.mp4" type="video/mp4"/>
+            <source src="https://cdn.mattlo.io/pluto.mp4" type="video/mp4" />
           </video>
         ) : (
-          <img src="https://cdn.mattlo.io/pluto.gif" className="embed-responsive-item fx"/>
+          <img src="https://cdn.mattlo.io/pluto.gif" className="embed-responsive-item fx" />
         )}
       </div>
 
