1db1cfc4899f559ced2cf4a3014656cf113278a3
Matt Lo <mattlo.developer@gmail.com>
2016-03-25 14:33:50 -0500

    fixed lint errors

--- src/client/components/QuickOfferings/QuickOfferings.js
+++ src/client/components/QuickOfferings/QuickOfferings.js
@@ -9,10 +9,11 @@ export default function QuickOfferings() {
           <div className="contents">
             <h3>Consultancy</h3>
             <p>
-              Modern companies hire me to figure out architecture, development effort, provide technical strategy on
-              domain-specific scenarios, and to act on new ideas to evolve their team to the next frontier.
-              I work side by side with the development team and work with leadership to ensure key performance
-              indicators are met and risk is managed to ensure deadlines.
+              Modern companies hire me to figure out architecture, development effort, provide
+              technical strategy on domain-specific scenarios, and to act on new ideas to evolve
+              their team to the next frontier. I work side by side with the development team and
+              work with leadership to ensure key performance indicators are met and risk is managed
+              to ensure deadlines.
             </p>
           </div>
         </div>
@@ -22,9 +23,10 @@ export default function QuickOfferings() {
           <div className="contents">
             <h3>Remote Code Reviews</h3>
             <p>
-              I provide customized reports that help determine technical debt, maintainability, ability to scale,
-              and suggestions to improve time-to-market. Your company provides me access to the codebase and you receive
-              your review and a video conference call outlining the items. All for a fixed cost.
+              I provide customized reports that help determine technical debt, maintainability,
+              ability to scale, and suggestions to improve time-to-market. Your company provides me
+              access to the codebase and you receive your review and a video conference call
+              outlining the items. All for a fixed cost.
             </p>
           </div>
         </div>
@@ -32,11 +34,11 @@ export default function QuickOfferings() {
           <div className="contents">
             <h3>Training and Workshops</h3>
             <p>
-              I work with agencies and product companies to help modernize their workflow. Often times teams are too
-              busy
-              to maintain their career training. I provide everything you need to know within a week (a few hours a
-              day).
-              Material will be in the form of customized content, code samples, verbal lectures, and Q&amp;As.
+              I work with agencies and product companies to help modernize their workflow.
+              Often times teams are too busy to maintain their career training. I provide everything
+              you need to know within a week (a few hours a day).
+              Material will be in the form of customized content, code samples, verbal lectures, and
+              Q&amp;As.
             </p>
           </div>
         </div>
