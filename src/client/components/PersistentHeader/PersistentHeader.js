0417ba021c3edc73a2c63d18e2bc720b6f6dd930
Matt Lo <mattlo.developer@gmail.com>
2016-03-24 19:33:10 -0500

    added mobile look and feel, added classnames dep for conditionals

--- src/client/components/PersistentHeader/PersistentHeader.js
+++ src/client/components/PersistentHeader/PersistentHeader.js
@@ -1,26 +1,69 @@
 import './PersistentHeader.scss';
 import React from 'react';
+import classnames from 'classnames';
 
-export default function PersistentHeader() {
-  return (
-    <div className="persistent-header">
-      <div className="container">
-        <div className="roi-text">
-          <i className="icon-javascript-alt" />
-          <span className="text-rotation">
-            Architecture
-          </span>
-          <span className="text-rotation">
-            Engineering
-          </span>
-          <span className="text-rotation">
-            Training
-          </span>
-          <span className="text-rotation">
-            Leadership
-          </span>
+export default class PersistentHeader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.rotationText = [
+      'Architecture',
+      'Engineering',
+      'Training',
+      'Leadership'
+    ];
+
+    this.state = {
+      rotationIndex: 0
+    };
+  }
+
+  componentDidMount() {
+    this.runRotationTimer();
+  }
+
+  runRotationTimer() {
+    setTimeout(() => {
+      this.runRotationTimer();
+      this.updateRotationText();
+    }, 1500);
+  }
+
+  updateRotationText() {
+    let rotationIndex = this.state.rotationIndex + 1;
+
+    // max validation override
+    if (this.state.rotationIndex + 1 >= this.rotationText.length) {
+      rotationIndex = 0;
+    }
+
+    this.setState({
+      rotationIndex
+    });
+  }
+
+  render() {
+    return (
+      <div className="persistent-header">
+        <div className="container">
+          <div className="roi-text">
+            <i className="icon-javascript-alt" />
+            <span className="text-rotation-container">
+              {this.rotationText.map((text, index) => (
+                <span
+                  key={index}
+                  className={classnames('text-rotation', {
+                  active: index === this.state.rotationIndex
+                })}
+                >
+                {text}
+              </span>
+              ))}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
