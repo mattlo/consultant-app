16a5e0ff8af2b9dab102317793fb8e13982abac7
Matt Lo <mattlo.developer@gmail.com>
2016-03-29 16:33:52 -0500

    added configurable hero image component

--- src/client/components/HeroImage/HeroImage.js
+++ src/client/components/HeroImage/HeroImage.js
@@ -1,20 +1,25 @@
 import './HeroImage.scss';
 import React from 'react';
 
-export default function HeroImage() {
+export default function HeroImage({src, children}) {
   return (
     <div className="hero-sales-pitch">
       <div className="video embed-responsive embed-responsive-16by9">
-        <img src="https://cdn.mattlo.io/pluto.jpg" className="embed-responsive-item" />
+        <img src={src} className="embed-responsive-item" />
       </div>
 
       <div className="contents">
         <div className="container">
           <h1 className="headline">
-            Test
+            {children}
           </h1>
         </div>
       </div>
     </div>
   );
 }
+
+HeroImage.propTypes = {
+  src: React.PropTypes.string.isRequired,
+  children: React.PropTypes.node
+};
