2567309ad957256c8fd3d5127bb8c542690cbe11
Matt Lo <mattlo.developer@gmail.com>
2016-04-01 12:59:54 -0500

    added footer content

--- src/client/components/Footer/Footer.js
+++ src/client/components/Footer/Footer.js
@@ -4,8 +4,29 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="row">
-
+      <div className="about-me">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 col-sm-6">
+              <p>
+                Contact me anytime at <strong>
+                <a href="mailto:mlo@mattlo.io">mlo@mattlo.io</a></strong> and lets work
+                together to discover the opportunities.
+              </p>
+            </div>
+            <div className="col-md-4 col-sm-6">
+              <p>
+                I'm a JavaScript architect consultant focusing on solution design, performance,
+                scalability, delivery, maintainability, and security.
+              </p>
+            </div>
+            <div className="col-md-4 col-xs-12">
+              <a className="availability">
+                I'm currently available for contracts up to 6 months.
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="company-info">
         <div className="container">
