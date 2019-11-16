65adbd7d823bced5e5bef73f2455fddc9d11f132
Matt Lo <mattlo.developer@gmail.com>
2019-11-16 16:59:25 -0600

    updated scp command

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,5 @@ fi
 endpoint="root@$1"
 scpEndpoint="$endpoint:app.tar.gz"
 tar -zcf app.tar.gz dist node_modules
-scp app.tar.gz $scpEndpoint
+scp -o StrictHostKeyChecking=no app.tar.gz $scpEndpoint
 ssh -oStrictHostKeyChecking=no $endpoint "forever stopall && rm -rf /opt/app && mkdir -p /opt/app && tar -xf app.tar.gz -C /opt/app && rm app.tar.gz && cd /opt && sh start.sh"
