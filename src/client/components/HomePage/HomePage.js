f2e7b7c9fb6212c6d0e8dca91fc7b92a54d23ea1
Matt Lo <mattlo.developer@gmail.com>
2019-11-16 16:55:31 -0600

    updated contact page

--- src/client/components/HomePage/HomePage.js
+++ src/client/components/HomePage/HomePage.js
@@ -15,35 +15,21 @@ export default function HomePage() {
     >
       <h1>Matt Lo</h1>
       <h3>
-        CTO @ <a href="https://www.innovadiv.com">Innovadiv.com</a>
+        Founder/CEO of <a href="https://getchipbot.com">ChipBot, Inc.</a>
+        <br />
+        Startup Advisor
       </h3>
       <hr />
-      <p style={{fontSize: '18px'}}>
-        If you came here for consulting, I no longer offer those services.
-        <br /><br />
-        If you came here to know more about me, read my tweets at
-        {' '}
-        <a href="https://www.twitter.com/Matt_Lo">
-          @Matt_Lo
-        </a>.
-        <br /><br />
-        If you came here to invest, DM
-        {' '}
-        <a href="https://www.twitter.com/Matt_Lo">
-          @Matt_Lo
-        </a>.
-        <br /><br />
-        If you came here for JavaScript expertise, open a StackOverflow link and DM the link to
-        {' '}
-        <a href="https://www.twitter.com/Matt_Lo">
-          @Matt_Lo
-        </a>.
-        <br /><br />
-        If you want to meet up in Chicago, DM
-        {' '}
-        <a href="https://www.twitter.com/Matt_Lo">
-          @Matt_Lo
-        </a>.
+      <p>
+        Twitter Account: <a href="https://www.twitter.com/Matt_Lo">https://www.twitter.com/Matt_Lo</a>
+      </p>
+      <hr />
+      <p>
+        Email: <a href="mailto:mlo@mattlo.io">mlo@mattlo.io</a>
+      </p>
+      <hr />
+      <p>
+        Cell Phone: DM on Twitter or email directly for this.
       </p>
     </div>
   );
